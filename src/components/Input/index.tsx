2b912700201256cf2da0af301b8b45d4e35b4c7f
Osaretin <osaretin.frank10@gmail.com>
2025-06-06 23:46:36 +0100

    updates

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,16 +2,24 @@ import React, { FC } from "react"
 
 interface InputI extends React.InputHTMLAttributes<HTMLInputElement> {
   prefixIcon?: React.ReactNode
+  suffixIcon?: React.ReactNode
   fullwidth?: boolean
 }
 
-const Input: FC<InputI> = ({ prefixIcon, className, fullwidth, ...props }) => {
+const Input: FC<InputI> = ({
+  prefixIcon,
+  className,
+  fullwidth,
+  suffixIcon,
+  ...props
+}) => {
   return (
     <div className="inline-block relative w-full">
       <input
         {...props}
         className={`
        bg-grey-700 text-grey-0 inline-block
+       ${suffixIcon ? "pr-[42px]" : "px-[16px]"}
       ${prefixIcon ? "px-[42px]" : "px-[16px]"} py-[12px] 
       ${
         fullwidth ? "w-full" : "w-[200px]"
@@ -23,6 +31,9 @@ const Input: FC<InputI> = ({ prefixIcon, className, fullwidth, ...props }) => {
       />
       <div className="absolute top-[50%] left-[16px] translate-y-[-50%]">
         {prefixIcon}
+      </div>
+      <div className="absolute top-[50%] right-[16px] translate-y-[-50%]">
+        {suffixIcon}
       </div>
     </div>
   )
